fec92fd6000a6721ebf2e3067c327f40cbf0b409
Martín Cepeda <mocepeda@uc.cl>
2019-11-20 08:40:28 +0100

    Update webcam.cpp

--- webcam.cpp
+++ webcam.cpp
@@ -56,7 +56,17 @@ void capture_and_show(int n)
 	}
 }
 
-void calibrate(Size s, Mat& cameraMatrix, Mat &distCoeffs)
+static void calcChessboardCorners(Size boardSize, float squareSize, vector<Point3f>& corners)
+{
+	corners.resize(0);
+
+	for (int i = 0; i < boardSize.height; i++)
+		for (int j = 0; j < boardSize.width; j++)
+			corners.push_back(Point3f(float((2 * j + i % 2) * squareSize),
+				float(i * squareSize), 0));
+
+}
+bool calibrate(Size s, Size imageSize, Mat& cameraMatrix, Mat &distCoeffs)
 {
 
 	VideoCapture cap(0);
@@ -101,11 +111,37 @@ void calibrate(Size s, Mat& cameraMatrix, Mat &distCoeffs)
 
 	cap.release();
 
+	/*-                                      */
+
 	cameraMatrix = Mat::eye(3, 3, CV_64F);
-
 	distCoeffs = Mat::zeros(8, 1, CV_64F);
+	vector<vector<Point3f> > objectPoints(1);
+	vector<Point3f> newObjPoints;
+	vector<vector<Point2f> > imagePoints;
 
+	vector<Mat> rvecs, tvecs;
 
+	float grid_width = 1.0f * (s.width - 1);
+
+	calcChessboardCorners(s, 1.0f, objectPoints[0]);
+	objectPoints[0][s.width - 1].x = objectPoints[0][0].x + grid_width;
+	newObjPoints = objectPoints[0];
+
+	objectPoints.resize(imagePoints.size(), objectPoints[0]);
+
+	double rms;
+	int iFixedPoint = -1;
+	
+	rms = calibrateCameraRO(objectPoints, imagePoints, imageSize, iFixedPoint, cameraMatrix, distCoeffs, rvecs, tvecs, newObjPoints, CALIB_FIX_K3 | CALIB_USE_LU);
+
+	bool ok = checkRange(cameraMatrix) && checkRange(distCoeffs);
+
+	objectPoints.clear();
+	objectPoints.resize(imagePoints.size(), newObjPoints);
+	totalAvgErr = computeReprojectionErrors(objectPoints, imagePoints,
+		rvecs, tvecs, cameraMatrix, distCoeffs, reprojErrs);
+
+	return ok;
 
 
 
@@ -125,7 +161,9 @@ int main(){
 	Size patternsize(4, 11); //number of centers
 	Mat cameraMatrix, distCoeffs;
 
-	calibrate(patternsize, cameraMatrix, distCoeffs);
+	Size imageSize;
+
+	calibrate(patternsize, imageSize, cameraMatrix, distCoeffs);
 	waitKey();
 
 	/*------ CALIBRATION -------*/
